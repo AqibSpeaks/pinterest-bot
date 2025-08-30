9c4c53030c3814d1fdd883016679644ae074dee6
AqibSpeaks <103169130+AqibSpeaks@users.noreply.github.com>
2025-08-31 01:54:01 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
-# config.py
-import os
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-PINTEREST_USERNAME = "pediacreative"
-PINTEREST_PASSWORD = "allloveyou123"
-BOARD_URL = "https://www.pinterest.com/pediacreative/"
-WEBSITE_URL = "https://www.linkedin.com/in/aqibramzan/?utm_source=pinterest&utm_medium=pin&utm_campaign=daily_post"
+[openai]
+api_key = "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"
+[pinterest]
+username = "pediacreative"
+password = "allloveyou123"
+board_url = "https://www.pinterest.com/pediacreative/"
+[website]
+url = "https://www.linkedin.com/in/aqibramzan/"
