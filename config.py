ec2d144372406d060b8cb5da1af900990352ef2b
AqibSpeaks <103169130+AqibSpeaks@users.noreply.github.com>
2025-08-30 22:58:51 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+OPENAI_API_KEY = "YOUR_OPENAI_API_KEY"
+PINTEREST_USERNAME = "pediacreative"
+PINTEREST_PASSWORD = "allloveyou123"
+BOARD_URL = "https://www.pinterest.com/pediacreative/"
+WEBSITE_URL = "https://www.linkedin.com/in/aqibramzan/?utm_source=pinterest&utm_medium=pin&utm_campaign=daily_post"
